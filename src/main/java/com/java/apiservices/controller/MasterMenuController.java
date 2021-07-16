ebd31856c75e98fd1c9d5a2899eb7f6ecf0bdc18
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2021-07-16 22:52:30 +0700

    add API crud master menu

--- src/main/java/com/java/apiservices/controller/MasterMenuController.java
+++ src/main/java/com/java/apiservices/controller/MasterMenuController.java
@@ -1,22 +1,114 @@
 package com.java.apiservices.controller;
 
 import com.java.apiservices.dto.MasterMenuDto;
-import com.java.apiservices.service.MasterMenuService;
+import com.java.apiservices.dto.Response;
+import com.java.apiservices.entity.MasterMenu;
+import com.java.apiservices.repository.MasterMenuRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/rest/master-menu")
+@Transactional
 public class MasterMenuController {
     @Autowired
-    private MasterMenuService masterMenuService;
+    MasterMenuRepository masterMenuRepository;
+    @Autowired
+    ModelMapper modelMapper;
+
 
     @GetMapping("/find-all")
-    public List<MasterMenuDto> findAll(@RequestParam String search,Pageable pageable) {
-        return masterMenuService.findAll(search, pageable);
+    public Response<Object> findAll(@RequestParam String search, Pageable pageable) {
+        Response<Object> response = new Response<>();
+
+        List<MasterMenuDto> masterMenuDtos = new ArrayList<>();
+        for (MasterMenu i : masterMenuRepository.findAll(search, pageable)) {
+            MasterMenuDto dto = modelMapper.map(i, MasterMenuDto.class);
+            masterMenuDtos.add(dto);
+        }
+
+        Map<String, Object> res = new HashMap<>();
+        res.put("items", masterMenuDtos);
+        res.put("totalItems", masterMenuRepository.findAll(search, null).size());
+        response.setSuccess(res);
+        return response;
+    }
+
+    @GetMapping("/find/{id}")
+    public Response<Object> findAll(@PathVariable String id) {
+        Response<Object> response = new Response<>();
+
+        Optional<MasterMenu> optionalEntity = masterMenuRepository.findById(id);
+        if (!optionalEntity.isPresent()) {
+            response.setError("ERR_MENU_ID_NOT_EXIST");
+            return response;
+        }
+        response.setSuccess(modelMapper.map(optionalEntity.get(), MasterMenuDto.class));
+        return response;
+    }
+
+    @PostMapping("/save")
+    public Response<String> save(@RequestBody MasterMenuDto req) {
+        Response<String> response = new Response<>();
+        MasterMenu masterMenu = new MasterMenu();
+        try {
+            masterMenu.setMenuName(req.getMenuName());
+            masterMenu.setMenuDescription(req.getMenuDescription());
+            masterMenu.setUrlImage(req.getUrlImage());
+            masterMenuRepository.save(masterMenu);
+            response.setSuccess("Master Menu successfully added!");
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public Response<String> update(@PathVariable String id, @RequestBody MasterMenuDto req) {
+        Response<String> response = new Response<>();
+        Optional<MasterMenu> optionalEntity = masterMenuRepository.findById(id);
+        if (!optionalEntity.isPresent()) {
+            response.setError("ERR_MENU_ID_NOT_EXIST");
+            return response;
+        }
+        try {
+            MasterMenu masterMenu = optionalEntity.get();
+            masterMenu.setMenuName(req.getMenuName());
+            masterMenu.setMenuDescription(req.getMenuDescription());
+            masterMenu.setUrlImage(req.getUrlImage());
+            masterMenuRepository.save(masterMenu);
+            response.setSuccess("Master Menu successfully edited!");
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Response<String> delete(@PathVariable String id) {
+        Response<String> response = new Response<>();
+        Optional<MasterMenu> optionalEntity = masterMenuRepository.findById(id);
+        if (!optionalEntity.isPresent()) {
+            response.setError("ERR_MENU_ID_NOT_EXIST");
+            return response;
+        }
+        try {
+            MasterMenu masterMenu = optionalEntity.get();
+            masterMenu.setIsDeleted((short) 1);
+            masterMenuRepository.save(masterMenu);
+            response.setSuccess("Master Menu successfully deleted!");
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
 
